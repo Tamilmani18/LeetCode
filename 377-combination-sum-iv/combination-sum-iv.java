b7ba9a6ebc75f6cbd70f888d1e9c02d94ed8cfee
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2024-05-07 18:15:02 +0530

    Time: 1 ms (76.89%) | Memory: 40.6 MB (83.39%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        
+        int[] dp = new int[target + 1];
+        // base case for 0, combination is 1
+        dp[0] = 1; 
+
+        // for every target 1 to n(target), iterating through nums array 
+        for ( int i = 1; i <= target; i++ ) {
+            
+            for ( int num : nums ) {
+                // picking numbers which <= target from nums arr
+                if ( num <= i ) { 
+                    dp[i] += dp[ i - num ]; // dp[i] + dp[target - picked number]
+                } 
+            }
+        }
+        return dp[target];
+    }
+}
