1ca376daa7cda60e5efe89a415f9ba17a3b684f9
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2024-10-15 13:13:28 +0530

    Update 0747-min-cost-climbing-stairs.java

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -17,3 +17,19 @@ class Solution {
         return Math.min(dp[n-1], dp[n-2]);
     }
 }
+
+// or
+
+    public static int minCostClimbingStairs(int[] cost) {
+
+        int first = cost[0];
+        int second = cost[1];
+
+        for (int i = 2; i < cost.length; i++) {
+            int curr = cost[i] + Math.min(first, second);
+            first = second;
+            second = curr;
+        }
+
+        return Math.min(first, second);
+    }
