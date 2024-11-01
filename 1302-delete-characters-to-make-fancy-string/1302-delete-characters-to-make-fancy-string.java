17f6a14858e3260bb4b28d37d3693c62aeaf81ce
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2024-11-01 16:54:25 +0530

    Time: 34 ms (53.49%), Space: 45.5 MB (80.4%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String makeFancyString(String s) {
+        
+        StringBuilder result = new StringBuilder();
+        
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            
+            if (i > 0 && s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count <= 2) {
+                result.append(s.charAt(i));
+            }
+        }
+        
+        return result.toString();
+    }
+}
