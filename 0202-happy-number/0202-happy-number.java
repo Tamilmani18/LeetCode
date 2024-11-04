0c334ae68405bef536feed0d7405e1ab1d69fa16
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2024-11-05 00:10:37 +0530

    Update 0202-happy-number.java

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -12,6 +12,36 @@ class Solution {
     
     public boolean isHappy(int n) {
             
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = calcSumOfSquareDigits(slow);
+            fast = calcSumOfSquareDigits(calcSumOfSquareDigits(fast));
+        } while (slow != fast);
+        
+        return slow == 1;
+    }
+ 
+}
+
+// TC -> O(n)
+// SC -> O(1)
+
+class Solution {
+
+    private int calcSumOfSquareDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    public boolean isHappy(int n) {
+            
         Set<Integer> set = new HashSet<>();
         while (n != 1 && !set.contains(n)) {
             set.add(n);
@@ -22,3 +52,6 @@ class Solution {
     }
  
 }
+
+// TC -> O(log n)
+// SC -> O(n)
