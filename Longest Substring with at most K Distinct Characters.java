23ca16c75d1eec133bdb1b8461ff5d1011b62697
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2024-11-07 10:18:32 +0530

    Update Longest Substring with at most K Distinct Characters.java

--- Longest Substring with at most K Distinct Characters.java
+++ Longest Substring with at most K Distinct Characters.java
@@ -1,6 +1,27 @@
 // Find the length of longest substring which has only k unique characters.
 
 
+    private static int lengthOfLongestSubstringKDistinct(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < s.length(); i++) {
+            map.clear();
+            for (int j = i; j < s.length(); j++) {
+                map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1 );
+                if (map.size() <= k) {
+                    max = Math.max(max, (j - i) + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+
+// TC -> O(n2)
+// SC -> O(n)
+
 public static int lengthOfLongestSubstringKDistinct(String s, int k) {
         int[] map = new int[256];
         int cnt = 0, max = 0; 
