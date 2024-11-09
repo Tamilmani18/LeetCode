146aa9666ec0b42a7a2db2915c1ac652e3470504
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2024-11-09 12:00:29 +0530

    Time: 2 ms (84.87%), Space: 44.4 MB (82.09%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    private static void dfs (int[] nums, List<List<Integer>> result, List<Integer> comb, int sum, int target, int start) {
+        if (sum == target) {
+            result.add(new ArrayList<>(comb));
+            return;
+        } else if (sum > target) {
+            return;
+        } else {
+            for (int i = start; i < nums.length; i++) {
+                comb.add(nums[i]);
+                dfs(nums, result, comb, sum + nums[i], target, i);
+                comb.remove(comb.size() - 1);
+            }
+        }
+    }
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        dfs(candidates, result, new ArrayList<>(), 0, target, 0);
+        
+        return result;
+    }
+}
+
+// TC -> O(2^k * n)
+// SC -> O(k)
