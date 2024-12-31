ba26de5c0426cbc2601826a42683fc9cb4cf6c1c
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2024-12-31 21:00:40 +0530

    Update 1463-the-k-weakest-rows-in-a-matrix.java

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -26,3 +26,59 @@ class Solution {
         return result;
     }
 }
+
+
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int[][] nums= new int[mat.length][2];
+        for(int i=0;i<mat.length;i++){
+            int n=0;
+            for(int j=0;j<mat[i].length;j++){
+                n=(mat[i][j]==1)? n+1:n;
+            }
+            nums[i][0]=n;
+            nums[i][1]=i;
+        }
+        Arrays.sort(nums,(a,b)->a[0]-b[0]);
+        int[] result=new int[k];
+        for(int i=0;i<k;i++){
+            result[i]=nums[i][1];
+        }
+        return result;
+    }
+}
+
+
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : b[1] - a[1]);
+        int[] ans = new int[k];
+        
+        for (int i = 0; i < mat.length; i++) {
+            pq.offer(new int[] {numOnes(mat[i]), i});
+            if (pq.size() > k)
+                pq.poll();
+        }
+        
+        while (k > 0)
+            ans[--k] = pq.poll()[1];
+        
+        return ans;
+    }
+    
+    private int numOnes(int[] row) {
+        int lo = 0;
+        int hi = row.length;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (row[mid] == 1)
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        
+        return lo;
+    }
+}
