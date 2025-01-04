47c7a81f40e2a9df98f20479bef0829633761ebb
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2025-01-04 14:43:32 +0530

    Update 2634-minimum-common-value.java

--- 2634-minimum-common-value/2634-minimum-common-value.java
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -10,3 +10,17 @@ class Solution {
         return -1;
     }
 }
+
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        for(int i = 0; i < nums1.length; i++){
+            int j = 0;
+            while( j < nums2.length && nums2[j] <= nums1[i] ){
+                if ( nums1[i] == nums2[j] ) return nums1[i];
+                j++;
+            }
+        }
+
+        return -1;
+    }
+}
