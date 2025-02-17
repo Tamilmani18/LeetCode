d121870dd060e58d3b566d8cbc713a7576fe1019
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2025-02-17 11:33:53 +0530

    Update 3sum.java

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,5 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> st = new HashSet<>();
+        
+        for(int i=0;i<nums.length;i++){
+            
+            Set<Integer> set = new HashSet<>();
+            
+            for(int j=i+1;j<nums.length;j++){
+                
+                int third = -(nums[i]+nums[j]);
+                
+                if(set.contains(third)){
+                    List<Integer> temp = Arrays.asList(nums[i],nums[j],third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                
+                set.add(nums[j]);
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
         
         List<List<Integer>> result = new ArrayList<>();
 
