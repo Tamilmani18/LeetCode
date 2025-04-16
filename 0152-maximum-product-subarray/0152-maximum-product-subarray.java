37d3695b1c151d05efb454372992c07ae2caddff
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2025-04-16 12:02:04 +0530

    Update 0152-maximum-product-subarray.java

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -20,3 +20,38 @@ class Solution {
         return maximumProduct;
     }
 }
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        
+        int n = nums.length;
+        
+        int ans = nums[0];
+
+        int min = 1 , max = 1;
+        
+        for( int i = 0; i < n; i++ ) {
+            // handling 0s
+            if ( nums[i] == 0 ) {
+                min = 1;
+                max = 1;
+                ans = Math.max( ans , 0 );
+            } else {
+
+                int temp = min;
+
+                // handling negative values to get next max
+                min = Math.min( nums[i], Math.min( nums[i]*min , nums[i]*max ) );
+
+                max = Math.max( nums[i], Math.max( nums[i]*temp , nums[i]*max ) );
+
+                ans = Math.max( ans , max );
+                
+            }
+        
+        }
+
+        return ans;
+        
+    }
+}
