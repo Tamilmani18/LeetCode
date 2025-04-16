19ab02fbb7abc3a255341a0ecd87949d3337f910
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2025-04-16 11:31:13 +0530

    Update 0238-product-of-array-except-self.java

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,4 +1,37 @@
 class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+        int n = nums.length;
+
+        int[] answer = new int[n];
+        
+        for ( int i = 0; i < n; i++ ) {
+            
+            int left = i-1;
+            
+            int right = i+1;
+
+            int product = 1;
+
+            while ( right < n ) {
+                product *= nums[right];
+                right++;
+            }
+
+            while ( left >= 0 ) {
+                product *= nums[left];
+                left--;
+            }
+            
+            answer[i] = product;
+        }
+
+        return answer;
+
+    }
+}
+
+class Solution {
     
     public int[] productExceptSelf(int[] nums) {
         
@@ -27,6 +60,37 @@ class Solution {
         }
 
         return answer;
+
+    }
+
+}
+
+
+class Solution {
+    
+    public int[] productExceptSelf(int[] nums) {
+        
+        int n = nums.length;
+
+        int[] prefixProduct = new int[n];
+
+        prefixProduct[0] = 1;
+        
+        for ( int i = 1; i < n; i++ ) {
+            prefixProduct[i] = prefixProduct[i-1] * nums[i-1];
+        }
+
+        int suffixProduct = 1;
+        
+        for ( int i = n-1; i >= 0; i-- ) {
+            
+            prefixProduct[i] = prefixProduct[i] * suffixProduct;
+
+            suffixProduct = suffixProduct * nums[i];
+
+        }
+
+        return prefixProduct;
 
     }
 
