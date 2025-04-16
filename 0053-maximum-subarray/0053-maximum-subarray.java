de31cad8f9ceb7e48225a6fd833fd23d224095e1
Tamil <144092813+Tamilmani18@users.noreply.github.com>
2025-04-16 11:36:35 +0530

    Update 0053-maximum-subarray.java

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,6 +4,31 @@ class Solution {
         
         int n = nums.length;
         
+        int maxSum = Integer.MIN_VALUE;
+        
+        for ( int i = 0; i < n; i++ ) {
+            
+            int currentSum = 0;
+            
+            for ( int j = i; j < n; j++ ) {
+                currentSum += nums[j];
+                maxSum = Math.max( currentSum, maxSum );
+            }
+
+        }
+        
+        return maxSum;
+    
+    }
+
+}
+
+class Solution {
+    
+    public int maxSubArray(int[] nums) {
+        
+        int n = nums.length;
+        
         int curSum = nums[0];
 
         int maxSum = nums[0];
